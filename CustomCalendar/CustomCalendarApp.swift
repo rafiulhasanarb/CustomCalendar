badbf978185604944b8cd25830518c9d3a2080c6
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-05-21 08:09:29 +0600

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Rafiul Hasan on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
